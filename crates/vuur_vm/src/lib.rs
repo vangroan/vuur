e5e5670b01021e3fc5548b11d4ed1e85e13980bc
Willem Victor <wimpievictor@gmail.com>
2023-02-24 22:41:26 +0200

    fixed VM

--- crates/vuur_vm/src/lib.rs
+++ crates/vuur_vm/src/lib.rs
@@ -1,11 +1,12 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use vuur_compile::bytecode::opcodes as ops;
+use vuur_compile::bytecode::{get_opcode, opcodes as ops, read_arg_a, read_arg_k};
 use vuur_compile::constants::CHUNK_HEADER_RESERVED;
 use vuur_compile::Chunk;
 
 pub const STRIDE: usize = 4;
+pub const END_OF_CHUNK: usize = std::usize::MAX;
 
 #[derive(Debug)]
 pub struct VM {
@@ -49,12 +50,12 @@ impl VM {
     pub fn run(&mut self, chunk: &Chunk) {
         match (*self.fiber).try_borrow_mut() {
             Ok(mut fiber) => {
-                fiber.ip = CHUNK_HEADER_RESERVED;
+                fiber.ip = 0;
                 fiber.run(chunk);
                 if fiber.done {
                     // Fiber is done executing, and cannot be resumed.
                     // TODO: Return top stack value
-                    println!("fiber is done");
+                    println!("fiber is done: {:?}", fiber.stack);
                 }
             }
             Err(err) => {
@@ -74,49 +75,92 @@ impl Fiber {
             ip: 0,
             stack: Vec::with_capacity(1024),
             // FIXME: For now the top level module function directs the interpreter to the starting byte to abort.
-            calls: vec![FrameInfo { return_addr: 0 }],
+            calls: vec![FrameInfo {
+                return_addr: END_OF_CHUNK,
+            }],
             done: false,
         }
     }
 
     pub fn run(&mut self, chunk: &Chunk) {
         'eval: loop {
-            let op = chunk.code()[self.ip];
+            if self.ip >= chunk.code().len() {
+                println!("end-of-chunk");
+                self.done = true;
+                break 'eval;
+            }
+            let instruction = chunk.code()[self.ip];
 
-            self.print_ip();
-            print!(
-                "{:02X} {:02X} {:02X} {:02X} ",
-                instruction[0], instruction[1], instruction[2], instruction[3]
-            );
+            {
+                let [o, a, b, c] = instruction.to_le_bytes();
+                self.print_ip();
+                print!("{:02X} {:02X} {:02X} {:02X}  ", o, a, b, c);
+            }
 
-            let op = instruction.get(0).cloned().unwrap_or(ops::NOOP);
+            let op = get_opcode(instruction);
+            // print!("opcode: {}", op);
 
             match op {
-                ops::NOOP => self.incr(),
+                ops::NOOP => {
+                    println!("");
+                    self.ip += 1
+                }
                 ops::ADD_I32 => {
-                    println!("add");
-                    self.incr()
+                    println!(".add");
+                    let b = self.stack.pop().unwrap_or_default() as i32;
+                    let a = self.stack.pop().unwrap_or_default() as i32;
+                    let c = a.wrapping_add(b);
+                    self.stack.push(c as u32);
+                    self.ip += 1
                 }
                 ops::SUB_I32 => {
-                    println!("sub");
-                    self.incr()
+                    println!(".sub");
+                    let b = self.stack.pop().unwrap_or_default() as i32;
+                    let a = self.stack.pop().unwrap_or_default() as i32;
+                    let c = a.wrapping_sub(b);
+                    self.stack.push(c as u32);
+                    self.ip += 1
                 }
                 ops::MUL_I32 => {
-                    println!("mul");
-                    self.incr()
+                    println!(".mul");
+                    let b = self.stack.pop().unwrap_or_default() as i32;
+                    let a = self.stack.pop().unwrap_or_default() as i32;
+                    let c = a.wrapping_mul(b);
+                    self.stack.push(c as u32);
+                    self.ip += 1
                 }
                 ops::PUSH_CONST => {
-                    let konst_idx = instruction.get(1).cloned().unwrap_or_default();
-                    println!("pushk {}", konst_idx);
-                    self.incr();
+                    // TODO: constant table
+                    let konst_idx = read_arg_k(instruction);
+                    println!(".pushk {}", konst_idx);
+                    self.ip += 1
+                }
+                ops::PUSH_CONST_IMM => {
+                    let konst = read_arg_a(instruction);
+                    println!(".pushi {}", konst);
+                    self.stack.push(konst as u32);
+                    self.ip += 1;
+                }
+                ops::FUNC => {
+                    println!(".function");
+                    self.ip += 2; // skip constant table
                 }
                 ops::RETURN => {
-                    let frame = self.calls.pop().unwrap_or_else(FrameInfo::default);
-                    println!("return to 0x{:04X}", frame.return_addr);
-                    self.ip = frame.return_addr;
+                    match self.calls.pop() {
+                        Some(frame) => {
+                            println!(".return to 0x{:06X}", frame.return_addr);
+                            self.ip = frame.return_addr;
+                        }
+                        None => {
+                            // abort
+                            println!(".abort");
+                            self.done = true;
+                            break 'eval;
+                        }
+                    }
                 }
                 _ => {
-                    println!("abort");
+                    println!(".abort");
                     self.done = true;
                     break 'eval;
                 }
@@ -125,11 +169,6 @@ impl Fiber {
     }
 
     fn print_ip(&self) {
-        print!("0x{:08X} ", self.ip);
-    }
-
-    #[inline(always)]
-    fn incr(&mut self) {
-        self.ip += STRIDE
+        print!("0x{:08X}  ", CHUNK_HEADER_RESERVED + self.ip);
     }
 }
